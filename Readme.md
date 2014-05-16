260d1ece75f26eb2c2282bbb8d1c25de72f9512a
Michael Leaney <leahcimic@gmail.com>
2014-05-16 13:20:19 +1000

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,55 @@
+Easy Inject
+-----------
+
+A very simple dependency injection library, inspired and mostly borrowed from Angular.
+
+## Installation
+
+```sh
+npm install --save easy-inject
+```
+
+## Usage
+
+Just like in Angular, there are two main ways to use the injection service. One is by declaring them
+as arguments in your function call. Easy Inject will then parse the argument names and supply the
+desired dependencies.
+
+```js
+var inject = require('easy-inject');
+
+var myFunc = function(foo, bar) {
+    console.log(foo, bar); // outputs "Hello world!"
+};
+
+inject(myFunc, {
+    foo: 'Hello ',
+    bar: 'world!'
+});
+```
+
+Or, you may supply an explicit array of dependencies, and they'll be passed to the function in the
+ same order as specified.
+
+ ```js
+var myFunc = ['foo', 'bar', function(a, b) {
+    console.log(a, b); // outputs "Hello world!"
+}];
+```
+
+## API
+
+Easy Inject exports one simple function which takes two arguments. The first is the function descriptor,
+which can be either a function, or an explicit array with values of the dependencies and the last element
+as a function.
+
+The second argument is a dictionary of injectable dependencies.
+
+When using inject, it will annotate the function with it's required dependencies, and it will resolve them
+by looking up the name in the dictionary of injectable dependencies.
+
+If the dependency cannot be found, an error will be thrown.
+
+## Todo
+
+* Unit tests
---
+++ inject.js
@@ -0,0 +1,72 @@
+/**
+ * Easy Inject: Dependency injection inspired and mostly borrowed from Angular
+ */
+
+var FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
+var FN_ARG_SPLIT = /,/;
+var FN_ARG = /^\s*(_?)(.+?)\1\s*$/;
+var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+
+function annotate(fn) {
+  var _dependencies,
+      functionText,
+      argumentsDeclaration,
+      last,
+      args,
+      iterator,
+      len;
+
+  if ((_dependencies = fn._dependencies)) {
+
+  } else if (typeof fn == 'function') {
+    _dependencies = [];
+    functionText = fn.toString().replace(STRIP_COMMENTS, '');
+    argumentsDeclaration = functionText.match(FN_ARGS);
+    args = argumentsDeclaration[1].split(FN_ARG_SPLIT);
+    len = args.length;
+    for (iterator = 0; iterator < len; iterator++) {
+      args[iterator].replace(FN_ARG, function(all, underscore, name){
+        _dependencies[_dependencies.length] = name;
+      });
+    }
+  } else if (Array.isArray(fn)) {
+    last = fn.length - 1;
+    if (typeof fn[last] !== 'function') {
+      throw new Error('DI: last array element was not a function');
+    }
+    _dependencies = fn.slice(0, last);
+  } else {
+    if (typeof fn !== 'function') {
+      throw new Error('Not a function');
+    }
+  }
+  fn._dependencies = _dependencies;
+  return _dependencies;
+};
+
+function inject(fn, injectables) {
+  var wants = annotate(fn),
+      i,
+      l = wants.length,
+      want,
+      injectVars = [];
+
+  for (i = 0; i < l; i++) {
+    if (typeof injectables[wants[i]] === 'undefined') {
+      throw new Error('Could not find ' + injectables[wants[i]]);
+    }
+    injectVars[injectVars.length] = injectables[wants[i]];
+  }
+
+  if (Array.isArray(fn)) {
+    fn = fn[fn.length-1];
+  }
+
+  if (typeof fn !== 'function') {
+    throw new Error('You asked me to do something stupid');
+  }
+
+  fn.apply(fn, injectVars);
+};
+
+module.exports = inject;
